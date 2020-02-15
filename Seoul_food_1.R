4699fc6d6e68796bf64005f8515aa889a4db52e2
Hoon0427 <ikaros977@gmail.com>
2020-02-15 13:19:48 +0000

    Seoul_food_1

---
+++ Seoul_food_1.R
@@ -0,0 +1,13 @@
+Call_chicken_01 <- read.csv("CALL_CHICKEN_01MONTH.csv")
+
+dim(Call_chicken_01)
+
+summary(Call_chicken_01)
+
+head(years3040)
+
+sum(fri30$통화건수)
+
+sum(fri40$통화건수)
+
+head(years3040[years3040$연령대 == "30대",])
