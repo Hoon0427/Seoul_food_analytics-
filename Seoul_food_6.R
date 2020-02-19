977debc382a4491b3d6c32a619b652c6da5c233a
Hoon0427 <ikaros977@gmail.com>
2020-02-19 12:32:17 +0000

    Seoul_food_fix

--- Seoul_food_6.R
+++ Seoul_food_6.R
@@ -1,11 +1,27 @@
 group_by_data[group_by_data$type=="chicken",]
 
-> group_by_data %>% 
-  +   group_by(wday) %>% 
-  +   filter(type == "chicken") %>% 
-  +   filter(date != "2019-01-07") %>%
-  +   filter(date != "2019-01-16") %>%
-  +   filter(date != "2019-01-22") %>%
-  +   filter(date != "2019-01-25") %>%
-  +   summarize(call = mean(call)) %>% 
-  +   as.data.frame()
+head(group_by_data)
+
+group_by_data %>% 
++   group_by("wday") %>% 
++   filter(type == "chicken") %>% 
++   filter(date != "2019-01-07") %>%
++   filter(date != "2019-01-16") %>%
++   filter(date != "2019-01-22") %>%
++   filter(date != "2019-01-25") %>%
++   summarize(call = mean(call)) %>% 
++   as.data.frame()
+
+group_by_data %>% 
++   group_by("wday") %>% 
++   filter(type == "피자") %>% 
++   filter(date != "2019-01-01") %>%
++   # filter(date != "2019-01-16") %>%
++   # filter(date != "2019-01-22") %>%
++   # filter(date != "2019-01-25") %>%
++   summarize(call = mean(call)) %>% 
++   as.data.frame()
+
+group_by_data %>% 
++   filter(type == "피자") %>% 
++   filter(date == "2019-01-01")
