12834f2fbd2fa95a836741ba3889ef8f323ea245
Hoon0427 <ikaros977@gmail.com>
2020-02-19 12:44:53 +0000

    Seoul_food_6_fix

--- Seoul_food_6.R
+++ Seoul_food_6.R
@@ -2,26 +2,27 @@ group_by_data[group_by_data$type=="chicken",]
 
 head(group_by_data)
 
-group_by_data %>% 
-+   group_by("wday") %>% 
-+   filter(type == "chicken") %>% 
-+   filter(date != "2019-01-07") %>%
-+   filter(date != "2019-01-16") %>%
-+   filter(date != "2019-01-22") %>%
-+   filter(date != "2019-01-25") %>%
-+   summarize(call = mean(call)) %>% 
-+   as.data.frame()
 
 group_by_data %>% 
-+   group_by("wday") %>% 
-+   filter(type == "피자") %>% 
-+   filter(date != "2019-01-01") %>%
-+   # filter(date != "2019-01-16") %>%
-+   # filter(date != "2019-01-22") %>%
-+   # filter(date != "2019-01-25") %>%
-+   summarize(call = mean(call)) %>% 
-+   as.data.frame()
+   group_by(wday) %>% 
+   filter(type == "chicken") %>% 
+   filter(date != "2019-01-07") %>%
+   filter(date != "2019-01-16") %>%
+   filter(date != "2019-01-22") %>%
+   filter(date != "2019-01-25") %>%
+   summarize(call = mean(call)) %>% 
+   as.data.frame()
 
 group_by_data %>% 
-+   filter(type == "피자") %>% 
-+   filter(date == "2019-01-01")
+   group_by(wday) %>% 
+   filter(type == "pizza") %>% 
+   filter(date != "2019-01-01") %>%
+   # filter(date != "2019-01-16") %>%
+   # filter(date != "2019-01-22") %>%
+   # filter(date != "2019-01-25") %>%
+   summarize(call = mean(call)) %>% 
+   as.data.frame()
+
+group_by_data %>% 
+   filter(type == "pizza") %>% 
+   filter(date == "2019-01-01")
